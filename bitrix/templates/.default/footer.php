e31421ce047f158a31f47cb3ca87a8a96f73b533
Eugene Voyner <60915715+Voynere@users.noreply.github.com>
2026-08-20 22:09:10 +1000

    Update footer links

--- bitrix/templates/.default/footer.php
+++ bitrix/templates/.default/footer.php
@@ -1,3 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
+
+<!-- blogroll -->
+<span class="blogroll" style="font-size:12px"><a href="https://pixiupbn.com">pixiupbn</a></span>
 </body>
 </html>
